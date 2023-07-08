86c347a0425ed4d1f53bc4b466ecf61b785bbcbd
SillyCookies <marissakoopman@live.nl>
2023-07-08 20:51:08 +0200

    Create function to display new scene. Put each line of text on new line.

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 function love.load()
 font = love.graphics.newFont(18)
 mybutton = {x = 200, y = 200, width = 80, height = 40, text = love.graphics.newText(font, "Begin")}
+scene = {draw = function()
+	love.graphics.print(introduction)
+	draw_button(mybutton)
+	end}
 end
 
 
@@ -8,8 +12,7 @@ function love.update(dt)
 end
 
 function love.draw()
-love.graphics.print(introduction)
-draw_button(mybutton)
+scene.draw()
 end
 
 function draw_button(button)
@@ -21,19 +24,46 @@ text_y = button_center_y - (button.text:getHeight()/2)
 love.graphics.draw(button.text, text_x, text_y)
 end
 
+function scene_switch(new_scene)
+scene = new_scene
+end
+
+
 
 function is_button_pressed(button, x, y)
 	if x >= button.x and x <= button.x+button.width and y >= button.y and y <= button.y+button.height then --Detect if the click was inside the button
-       button.clicked = true --This is what triggers the pop-up image
+		return true --This is what triggers the pop-up image
     end
 end		
 		
 
 function love.mousepressed(x, y, button ,istouch)
     if button == 1 then --Left click
-        is_button_pressed(mybutton, x, y)
-			
+        if is_button_pressed(mybutton, x, y) then
+			scene_switch(cafe_intro)
+		end	
     end
 end
 
-introduction = "This is a story of two people meeting, and how their relationship develops. You control the music driving their story."
+introduction = {"This is a story of two people meeting, and how their relationship develops. You control the music driving their story."}
+
+cafe_intro = {draw = function()
+			yoffset= 0
+			for i , line in ipairs(cafe_intro) do
+				
+				line_text = love.graphics.newText(font, line)
+				love.graphics.draw(line_text, 0, yoffset)
+			yoffset = yoffset + line_text:getHeight() 
+			end
+			end,
+			
+			"SAM sits in a cafe, at the window, watching people walk by.",
+			"SAM sips the coffee as ALEX walks in. The coffee is terrible",
+			"ALEX: Sam?", 
+			"SAM: Alex, Hi!", 
+			"ALEX sits down.",
+			"ALEX: It’s great to finally meet you!"
+
+}
+
+
